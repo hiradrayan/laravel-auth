19e87c10f4231d9b517036ce4662c0edb273437f
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-04-22 14:35:15 +0330

    add service provider

---
+++ src/Authentication.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace Authentication; 
+
+class Authentication {
+    
+}
---
+++ src/AuthenticationServiceProvider.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Authentication;
+
+use Illuminate\Support\ServiceProvider;
+
+class AuthenticationServiceProvider extends ServiceProvider
+{
+        /**
+     * Perform post-registration booting of services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+
+        $this->loadRoutesFrom(__DIR__.'/path/routes/routes.php');
+        $this->loadViewsFrom(__DIR__.'/path/views', 'auth');
+
+        // $this->publishes([
+        //     __DIR__.'/path/views' => resource_path('views/vendor/courier'),
+        // ]);
+
+        // $this->publishesMigrations([
+        //     __DIR__.'/../database/migrations' => database_path('migrations'),
+        // ]);
+    }
+
+    /**
+     * Register any package services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // $this->mergeConfigFrom(__DIR__.'/../config/authentication.php', 'authentication');
+
+        // Register the service the package provides.
+        $this->app->singleton('authentication', function ($app) {
+            return new Authentication;
+        });
+    }
+}
