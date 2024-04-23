6d88970a91f23db963e4d6e0adf18fbf3ef25f05
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-04-23 14:57:22 +0330

    add test file

---
+++ tests/Tests.php
@@ -0,0 +1,16 @@
+<?php
+
+use Orchestra\Testbench\TestCase;
+
+class Tests extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
