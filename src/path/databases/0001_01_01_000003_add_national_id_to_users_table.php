44760c114543ee32e05698ee345a634f3fb8df66
reyhane <97376622+reyhane1376@users.noreply.github.com>
2024-05-01 19:05:53 +0330

    change migration place

--- src/path/databases/0001_01_01_000003_add_national_id_to_users_table.php
+++ src/path/databases/0001_01_01_000003_add_national_id_to_users_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->string('national_id', 10)->unique();
+            $table->string('national_id', 10)->unique()->after('email');
         });
     }
 
